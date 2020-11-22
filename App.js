0d6238b9404e052398ba1bb866533135571b00e0
이수진_세부내용 작성 <email@sujinmail.com>
2020-11-22 11:19:02 -0500

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,166 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Platform, FlatList, Image } from "react-native";
+import styled from "styled-components/native";
+import Constants from "expo-constants";
+import _ from "lodash";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import produce from "immer";
+
+AsyncStorage.getItem("list")
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(error.massage);
+  });
+// AsyncStorage.setItem("list", "list value")
+//   .then(() => {
+//     console.log("saved");
+//   })
+//   .catch((error) => console.log(error.massage));
+
+const Container = styled.SafeAreaView`
+  flex: 1;
+`;
+const Contents = styled.View`
+  flex: 1;
+`;
+const InputContainer = styled.View`
+  flex-direction: row;
+  padding: 8px;
+`;
+const Input = styled.TextInput`
+  border: 1px solid #e5e5e5;
+  flex: 1;
+`;
+
+const KeyboardAvoidingView = styled.KeyboardAvoidingView`
+  flex: 1;
+  padding-top: ${Constants.statusBarHeight}px;
+`;
+
+const Button = styled.Button``;
+
+const TodoItem = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-right: 10px;
+`;
+const TodoItemText = styled.Text`
+  font-size: 20px;
+  flex: 1;
+  padding: 10px;
+`;
+const TotoItemButton = styled.Button`
+  align-items: flex-end;
+`;
+
+const Check = styled.TouchableOpacity`
+  margin-right: 4px;
+  margin-left: 10px;
+`;
+
+const IconImage = styled.Image`
+  width: 60px;
+  height: 60px;
+`;
+
+const Title = styled.Text`
+  font-size: 30px;
+  color: dodgerblue;
+  margin: 20px auto;
+  border: 1px solid dodgerblue;
+`;
 
 export default function App() {
+  const [list, setList] = useState([]);
+  const [inputTodo, setInputTodo] = useState("");
+  useEffect(() => {
+    AsyncStorage.getItem("list")
+      .then((data) => {
+        if (data !== null) {
+          setList(JSON.parse(data));
+        }
+      })
+      .catch((error) => console.log(error.massage));
+  }, []);
+
+  const store = (newList) => {
+    setList(newList);
+    AsyncStorage.setItem("list", JSON.stringify(newList));
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <Title> ♥️ 오늘의 할일 ♥️ </Title>
+        <Contents>
+          <FlatList
+            data={list}
+            renderItem={({ item }) => (
+              <TodoItem>
+                <Check
+                  onPress={() => {
+                    setList(
+                      produce(list, (draft) => {
+                        const index = list.indexOf(item);
+                        draft[index].done = !draft[index].done;
+                      })
+                    );
+                  }}
+                >
+                  {item.done ? (
+                    <IconImage source={require("./assets/jin.png")} />
+                  ) : (
+                    <IconImage source={require("./assets/chan.png")} />
+                  )}
+                </Check>
+                <TodoItemText>{item.todo}</TodoItemText>
+                <TotoItemButton
+                  color="pink"
+                  title=" 삭제 "
+                  onPress={() => {
+                    store(_.reject(list, (element) => element.id === item.id));
+                  }}
+                />
+              </TodoItem>
+            )}
+          />
+        </Contents>
+        <InputContainer>
+          <Input
+            value={inputTodo}
+            placeholder="오늘 해야 할 일을 적어보삼"
+            onChangeText={(value) => setInputTodo(value)}
+          />
+          <Button
+            title="추가"
+            onPress={() => {
+              if (inputTodo === "") {
+                return;
+              }
+              const newItem = {
+                id: new Date().getTime().toString(),
+                todo: inputTodo,
+                done: false,
+              };
+              store([...list, newItem]);
+              setInputTodo("");
+            }}
+          />
+        </InputContainer>
+      </KeyboardAvoidingView>
+    </Container>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
